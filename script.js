6d9d990fe33ae90dc28d5c0b4e7a9de040e2cf7c
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-02-21 17:28:34 +0100

    color

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ console.log(firstName);
 const secondName = prompt("Quel'è il tuo cognome?");
 console.log(secondName);
 
+const color = prompt("Qual'è il tuo colore preferito?");
+console.log(color);
+
 /* let password = +++ */
